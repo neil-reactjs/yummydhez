dda1dfc61b6556e140a3ea28dc6f5fe10957243e
neilien <neil.laravel@gmail.com>
2023-03-08 17:38:40 +0800

    modified css and rotate animation in app.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <div className = "App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          20230308 <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="tel:+639997504533"
-          // target="_blank"
-          // rel="noopener noreferrer"
-        >
-          Call Me
-        </a>
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+         <a
+            className="App-link"
+           href="tel:+639997504533"
+           // target="_blank"
+           // rel="noopener noreferrer"
+         >
+           Call Me
+         </a>
+        </header>
     </div>
   );
 }
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           20230308 <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="tel:+639997504533"
+//           // target="_blank"
+//           // rel="noopener noreferrer"
+//         >
+//           Call Me
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;

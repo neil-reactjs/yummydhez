5112f651f67d3aa8003eb878a6fee8e8e8840a2b
neilien <neil.laravel@gmail.com>
2023-03-12 18:03:16 +0800

    added tailwind

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,9 @@
 const Footer = () => {
     return (
         <>
-        <h1>Footer</h1>
+    <h1 className="text-3xl font-bold underline bg-gray-900 text-pink-200">
+      Hello world!
+    </h1>
 
         </>
     );
